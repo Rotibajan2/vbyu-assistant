770e312743173a805673c6409eac3ef62c9cae83
Rotibajan2 <126740303+Rotibajan2@users.noreply.github.com>
2025-09-08 01:46:55 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
-export const config = { matcher: ["/chat.js", "/vybu-chat.js", "/vbyu-chat.js"] };
+
+export const config = {
+  matcher: ["/chat.js", "/vybu-chat.js", "/vbyu-chat.js", "/vbyu-chat-v2.js"],
+};
+
 export default function middleware(req: NextRequest) {
-  const url = new URL("/vbyu-chat-v2.js", req.url);
-  const res = NextResponse.rewrite(url);
-  res.headers.set("Cache-Control","no-store, no-cache, must-revalidate, max-age=0");
-  res.headers.set("Pragma","no-cache");
-  return res;
+  const url = new URL("/scripts/vbyu-chat-v2", req.url);
+  return NextResponse.rewrite(url);
 }
